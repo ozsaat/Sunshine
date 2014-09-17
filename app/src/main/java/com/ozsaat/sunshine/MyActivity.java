29cbba9e910a5543fb32fe7a758a36f141c36482
Oz <oss202@gmail.com>
2014-09-17 16:02:26 +0100

    Errors. Errors everywhere.

--- app/src/main/java/com/ozsaat/sunshine/MyActivity.java
+++ app/src/main/java/com/ozsaat/sunshine/MyActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -80,6 +81,10 @@ public class MyActivity extends ActionBarActivity {
                             R.layout.list_item_forecast,
                             R.id.list_item_forecast_textview,
                             weekForecast);
+
+            ListView listview = (ListView) rootView.findViewById(
+                    R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
 
             return rootView;
         }
